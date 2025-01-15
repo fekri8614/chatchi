4133ccec1c9add445c8d0436c4e0ea84a70d3b4b
Mohammad Reza Fekri <92860582+fekri8614@users.noreply.github.com>
2025-01-15 19:57:41 +0330

    edited `main.dart`

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ChatChi',
       theme: appTheme(),
       debugShowCheckedModeBanner: false,
       home: const ChatScreen(),
